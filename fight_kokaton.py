33eb8effe87489622112428fe9fc17d2a2c96012
c0a23039 <c0a23039f2@edu.teu.ac.jp>
2025-10-07 14:28:49 +0900

    ren3

--- fight_kokaton.py
+++ fight_kokaton.py
@@ -171,6 +171,7 @@ def main():
                 if beam.rct.colliderect(bomb.rct):
                     # bomb is out of game.
                     beam, bomb  = None, None
+                    bird.change_img(6, screen)
 
         key_lst = pg.key.get_pressed()
         bird.update(key_lst, screen)
